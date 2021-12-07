150300c899590990a5d742bfc0432cb0c70cfd39
Tommaso Crippa <crippa.tommaso@gmail.com>
2021-12-07 14:58:40 +0100

    Added day 7

---
+++ day7.py
@@ -0,0 +1,47 @@
+
+# https://adventofcode.com/2021/day/1
+
+# Used for Part 2
+def summarial(value): # Calculating factorial but using + instead of *
+    count = 0
+    while value != 0:
+        count += value
+        value -= 1
+    return count # Recursion not possible because numbers given are too big (with 1000+ function calls the program stops)
+
+
+def main():
+
+    input_data = open("day7_input.txt", "r") # Getting data and cleaning it
+    nums = [int(i) for i in input_data.readline().split(",")]
+
+    # N.B Part 1 and 2 done in parallel
+
+    minimum1 = float("+inf") # Starting with maximum value possible as minimum
+    minimum2 = float("+inf")
+
+    for i in range(max(nums)):
+
+        tmp1 = 0 
+        tmp2 = 0
+        for j in nums: # Calculating the amount of fuel needed to get to align:
+            tmp1 += abs(i-j) # using human engineering
+            tmp2 += summarial(abs(i-j)) # using crab engineering
+
+            if tmp1 > minimum1 and tmp2 > minimum2: # If both tmps are already greater than their minimum don't bother continuing
+                break
+
+        if minimum1 > tmp1: # If tmp is lower than minimum, replace iti
+            minimum1 = tmp1
+        if minimum2 > tmp2:
+            minimum2 = tmp2
+
+    # Total seconds in loop: about 50 seconds (could be better)
+
+    print(f"{minimum1} fuel was used to align using human engineering\n") # 336040
+    print(f"{minimum2} fuel was used to align using crab engineering") # 94813675
+
+
+
+if __name__ == "__main__":
+    main()
